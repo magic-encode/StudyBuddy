3c9f6702483a0157d352d2ed9a4d4c014803dbc1
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-01-09 14:09:52 +0500

    Added room_form.html

--- base/models.py
+++ base/models.py
@@ -11,7 +11,7 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
