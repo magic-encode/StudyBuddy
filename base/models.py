33253ee739b5cc8df2bb16574180f866cb685445
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-01-09 08:42:17 +0500

    Sudy Buddy cool project

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
--- base/views.py
+++ base/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms, }
     return render(request, 'home.html', context)  
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     
     context = {'room': room}
     return render(request, 'room.html', context)
