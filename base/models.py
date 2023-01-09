d731420eece733f405a0770468ed49f9c4985009
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-01-09 10:46:58 +0500

    Updated room detail

--- base/models.py
+++ base/models.py
@@ -1,15 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Room(models.Model):
-    # topic = 
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     date = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self) -> str:
         return self.name
-    
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:20]
