a1b5bc8f533a7465b24337fac4747e12d4bee22b
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-01-12 05:16:51 +0500

    Added login and register codes

--- base/models.py
+++ base/models.py
@@ -13,6 +13,7 @@ class Topic(models.Model):
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
     updated = models.DateTimeField(auto_now=True)
--- base/views.py
+++ base/views.py
@@ -93,6 +93,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-date')
+    participants = room.participants.all()
     
     if request.method == 'POST':
         message = Message.objects.create(
@@ -100,9 +101,10 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body'),
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    
-    context = {'room': room, 'room_messages': room_messages}
+     
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'room.html', context)
 
 
