d7ef079289f1215c141b6b38b06bdbc0bfb987fd
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-01-15 13:56:55 +0500

    Fixing api codes

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import getRoutes
+from .views import getRoutes, getRooms, getRoom
 
 
 
 urlpatterns = [
     path('', getRoutes, name='routes'),
-    
+    path('rooms/', getRooms),
+    path('rooms/<str:pk>/', getRoom),
 ]
 
+ 
--- base/api/views.py
+++ base/api/views.py
@@ -1,8 +1,12 @@
-from rest_framework.decorators import api_view
+from base.models import Room
+
+from .serializers import RoomSerializer
+
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
-
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         'GET /api',
@@ -10,4 +14,21 @@ def getRoutes(request):
         'GET /api/rooms/:id',
     ]
     
-    return Response(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    
+    return Response(serializer.data)
+
